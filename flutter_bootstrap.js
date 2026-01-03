ad111792cee0dde2e57d7430c5a68a48eada4bf6
Samet Arslan <128809851+SametArslan01@users.noreply.github.com>
2026-01-03 17:58:23 +0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4263168110"
+    serviceWorkerVersion: "980168134"
   }
 });
