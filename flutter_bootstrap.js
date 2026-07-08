8f9ca23f6fdb3af0b2d49c1af35491c7392ac6b1
Samet Arslan <smtrlsn84@gmail.com>
2026-07-08 15:04:50 +0300

    Add home_map.png for PetFeedMap

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3373052237" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2840554842" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
